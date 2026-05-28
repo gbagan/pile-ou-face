5a377be913d5267687380cf5ff6ef243c8a155ca
Guillaume Bagan <gbagan@gmail.com>
2026-05-28 14:16:40 +0200

    nettoyage du code

--- src/lib/Draw.svelte.ts
+++ src/lib/Draw.svelte.ts
@@ -1,10 +1,10 @@
 import { Markov } from "./Markov";
 
 export class Draw {
-  coins: (0 | 1)[] = $state([]);
+  #coins: (0 | 1)[] = $state([]);
   #lastTailsSequence = 0;
   #lastHeadsSequence = 0;
-  largestSequence = $state.raw(0);
+  #largestSequence = $state.raw(0);
   #headsCount = $state.raw(0);
   #predictedCount = $state.raw(0);
   #markov1: Markov;
@@ -12,9 +12,17 @@ export class Draw {
   #markov3: Markov;
 
   constructor() {
-    this.#markov1 = new Markov(1, this.coins);
-    this.#markov2 = new Markov(2, this.coins);
-    this.#markov3 = new Markov(3, this.coins);
+    this.#markov1 = new Markov(1, this.#coins);
+    this.#markov2 = new Markov(2, this.#coins);
+    this.#markov3 = new Markov(3, this.#coins);
+  }
+
+  get coins(): readonly (0 | 1)[] {
+    return this.#coins;
+  }
+
+  get largestSequence(): number {
+    return this.#largestSequence;
   }
 
   #predict(): 0 | 1 {
@@ -29,43 +37,40 @@ export class Draw {
     this.#markov1.update(coin);
     this.#markov2.update(coin);
     this.#markov3.update(coin);
-    this.coins.push(coin);
+    this.#coins.push(coin);
     if (coin === 0) {
       this.#headsCount += 1;
       this.#lastHeadsSequence += 1;
-      this.largestSequence = Math.max(this.largestSequence, this.#lastHeadsSequence)
+      this.#largestSequence = Math.max(this.#largestSequence, this.#lastHeadsSequence)
       this.#lastTailsSequence = 0;
     } else {
       this.#lastHeadsSequence = 0;
       this.#lastTailsSequence += 1;
-      this.largestSequence = Math.max(this.largestSequence, this.#lastTailsSequence)
+      this.#largestSequence = Math.max(this.#largestSequence, this.#lastTailsSequence)
     }
   }
 
   reset() {
-    this.coins.length = 0;
+    this.#coins.length = 0;
     this.#lastTailsSequence = 0;
     this.#lastHeadsSequence = 0;
     this.#headsCount = 0;
     this.#predictedCount = 0;
-    this.largestSequence = 0;
+    this.#largestSequence = 0;
     this.#markov1.reset();
     this.#markov2.reset();
     this.#markov3.reset();
   }
 
   headsRate() {
-    return this.coins.length === 0 ? 0.5 : this.#headsCount / this.coins.length;
+    return this.#coins.length === 0 ? 0.5 : this.#headsCount / this.#coins.length;
   }
 
   longRunRate() {
-    if (this.coins.length <= 1) {
-      return 0.5;
-    }
-    return this.largestSequence / Math.min(17, this.coins.length);
+    return this.#coins.length <= 1 ? 0.5 : this.#largestSequence / Math.min(17, this.#coins.length);
   }
 
   predictedRate() {
-    return this.coins.length === 0 ? 0.5 : this.#predictedCount / this.coins.length;
+    return this.#coins.length === 0 ? 0.5 : this.#predictedCount / this.#coins.length;
   }
 }
