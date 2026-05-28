ac89dffa8015fbc81b4853f7699aed2c3fd2b459
Guillaume Bagan <gbagan@gmail.com>
2026-05-28 11:39:18 +0200

    règle un problème dans la prédiction de pile ou face

--- src/lib/Draw.svelte.ts
+++ src/lib/Draw.svelte.ts
@@ -1,43 +1,4 @@
-class Markov {
-  #coins: (0 | 1)[];
-  #order: number;
-  #table: Record<string, [number, number]> = {};
-
-  constructor(order: number, coins: (0 | 1)[]) {
-    this.#coins = coins;
-    this.#order = order;
-  }
-
-  reset() {
-    this.#table = {};
-  }
-
-  #key() {
-    if (this.#coins.length < this.#order) return null;
-    return this.#coins.slice(-this.#order).join('');
-  }
-
-  update(coin: 0 | 1) {
-    const key = this.#key();
-    if (!key) return;
-    const table = this.#table;
-    if (!table[key]) table[key] = [0, 0];
-    table[key][coin]++;
-  }
-
-  predict(): 0 | 1 | null {
-    const key = this.#key();
-    if (!key) return null;
-    const table = this.#table;
-    if (!table[key]) return null;
-    const p0 = table[key][0];
-    const p1 = table[key][1];
-    const t = p0 + p1;
-    if (t === 0) return null;
-    return p1 >= 0.5 * t ? 1 : 0;
-  }
-}
-
+import { Markov } from "./Markov";
 
 export class Draw {
   coins: (0 | 1)[] = $state([]);
@@ -65,10 +26,10 @@ export class Draw {
     if (coin === prediction) {
       this.#predictedCount += 1;
     }
-    this.coins.push(coin);
     this.#markov1.update(coin);
     this.#markov2.update(coin);
     this.#markov3.update(coin);
+    this.coins.push(coin);
     if (coin === 0) {
       this.#headsCount += 1;
       this.#lastHeadsSequence += 1;
---
+++ src/lib/Markov.ts
@@ -0,0 +1,39 @@
+export class Markov {
+  #coins: (0 | 1)[];
+  #order: number;
+  #table: Record<string, [number, number]> = {};
+
+  constructor(order: number, coins: (0 | 1)[]) {
+    this.#coins = coins;
+    this.#order = order;
+  }
+
+  reset() {
+    this.#table = {};
+  }
+
+  #key() {
+    if (this.#coins.length < this.#order) return null;
+    return this.#coins.slice(-this.#order).join('');
+  }
+
+  update(coin: 0 | 1) {
+    const key = this.#key();
+    if (!key) return;
+    const table = this.#table;
+    if (!table[key]) table[key] = [0, 0];
+    table[key][coin]++;
+  }
+
+  predict(): 0 | 1 | null {
+    const key = this.#key();
+    if (!key) return null;
+    const table = this.#table;
+    if (!table[key]) return null;
+    const p0 = table[key][0];
+    const p1 = table[key][1];
+    const t = p0 + p1;
+    if (t === 0) return null;
+    return p1 >= 0.5 * t ? 1 : 0;
+  }
+}
